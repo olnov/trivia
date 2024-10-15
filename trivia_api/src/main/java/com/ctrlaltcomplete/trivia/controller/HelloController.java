2ec92c49c9672fa2b0dd83ba3cbe489fa97b5890
Harry <hmsh2000@gmail.com>
2024-10-15 13:35:37 +0100

    Welcome

--- trivia_api/src/main/java/com/ctrlaltcomplete/trivia/controller/HelloController.java
+++ trivia_api/src/main/java/com/ctrlaltcomplete/trivia/controller/HelloController.java
@@ -1,7 +1,6 @@
 package com.ctrlaltcomplete.trivia.controller;
 
 import jakarta.servlet.http.HttpSession;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -9,7 +8,17 @@ import org.springframework.web.bind.annotation.RestController;
 public class HelloController {
 
     @GetMapping("/")
-    public String sayHello (HttpSession session) {
-        return "Hello from trivia" + session.getId();
+    public String sayHello(HttpSession session) {
+        return "<html>" +
+                "<body>" +
+                "<h1>Welcome young Quizzard!</h1>" +
+                "<p>Session ID: " + session.getId() + "</p>" +
+                "<p><strong>Welcome to Quiznados!</strong></p>" +
+                "<p>Well done on logging in and creating your profile.</p>" +
+                "<p>When ready, click <strong>begin</strong> to start your journey to become a quizzard.</p>" +
+                "<p>Alternatively, see your score on the <strong>leaderboard</strong>.</p>" +
+                "<p>Stay tuned for new changes soon!</p>" +
+                "</body>" +
+                "</html>";
     }
 }
