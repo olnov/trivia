a8caec2527b86b0109ac6a21da71af6323be6aa4
Harry <hmsh2000@gmail.com>
2024-10-15 15:10:29 +0100

    about to merge

--- trivia_api/src/main/java/com/ctrlaltcomplete/trivia/controller/HelloController.java
+++ trivia_api/src/main/java/com/ctrlaltcomplete/trivia/controller/HelloController.java
@@ -14,7 +14,7 @@ public class HelloController {
                 "<h1>Welcome young Quizzard!</h1>" +
                 "<p>Session ID: " + session.getId() + "</p>" +
                 "<p><strong>Welcome to Quiznados!</strong></p>" +
-                "<p>Well done on logging in and creating your profile.</p>" +
+                "<p>Well done on logging in and creating your profile. </p>" +
                 "<p>When ready, click <strong>begin</strong> to start your journey to become a quizzard.</p>" +
                 "<p>Alternatively, see your score on the <strong>leaderboard</strong>.</p>" +
                 "<p>Stay tuned for new changes soon!</p>" +
