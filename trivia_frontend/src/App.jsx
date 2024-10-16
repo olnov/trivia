2eaf97c7032fce6497bae8b48c080e9b3cf5ed3a
Oleg Novikov <o.novikov@ymail.com>
2024-10-16 10:37:18 +0100

    Static pahe example

--- trivia_frontend/src/App.jsx
+++ trivia_frontend/src/App.jsx
@@ -4,11 +4,16 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 import Users from './pages/Users';
+import Home from './pages/Home';
 
 const router = createBrowserRouter([
   {
       path: "/users",
       element: <Users />,
+  },
+  {
+    path: "/home",
+    element: <Home />,
   }
 ]);
 
