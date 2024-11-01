334260b92742db40da94b950b4974aaf9fd6100d
Mazen Magzoub <mazenmagzoub@gmail.com>
2024-11-01 15:13:27 +0000

    Connecting socket-io client

--- trivia_node/app.js
+++ trivia_node/app.js
@@ -1,44 +1,41 @@
-const express = require("express");
-const http = require("http");
-const { Server } = require("socket.io");
-const cors = require("cors");
+var createError = require("http-errors");
+var express = require("express");
+var path = require("path");
+var cookieParser = require("cookie-parser");
+var logger = require("morgan");
 
-const app = express();
-app.use(cors());
-const server = http.createServer(app);
-const io = new Server(server);
+var indexRouter = require("./routes/index");
+var usersRouter = require("./routes/users");
 
-// Define port
-const PORT = process.env.PORT || 3001;
+var app = express();
 
-// Serve the React app or any other frontend from a 'public' directory if needed
-app.use(express.static("public"));
+// view engine setup
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "jade");
 
-// WebSocket connection handling
-io.on("connection", (socket) => {
-  console.log("A user connected:", socket.id);
+app.use(logger("dev"));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, "public")));
 
-  // Join a room for multiplayer game sessions
-  socket.on("joinGame", (roomId) => {
-    socket.join(roomId);
-    console.log(`User ${socket.id} joined room: ${roomId}`);
-  });
+app.use("/", indexRouter);
+app.use("/users", usersRouter);
 
-  // Handle game data exchange, e.g., sending answers
-  socket.on("playerMove", (data) => {
-    const { roomId, move } = data;
-    socket.to(roomId).emit("updateGame", { playerId: socket.id, move });
-  });
-
-  // Disconnect handling
-  socket.on("disconnect", () => {
-    console.log("User disconnected:", socket.id);
-  });
+// catch 404 and forward to error handler
+app.use(function (req, res, next) {
+  next(createError(404));
 });
 
-// Start server
-app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
+// error handler
+app.use(function (err, req, res, next) {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get("env") === "development" ? err : {};
+
+  // render the error page
+  res.status(err.status || 500);
+  res.render("error");
 });
 
 module.exports = app;
